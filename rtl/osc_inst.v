3d368d96ff5a3fb69e10d757b04edd30c1ba4618
Patrick Urban <patrick.urban@web.de>
2025-01-23 00:03:33 +0100

    rtl/top*: add proper synchronization between clock domains

--- rtl/osc_inst.v
+++ rtl/osc_inst.v
@@ -1,15 +1,19 @@
 `timescale 1ns / 1ps
 
 module osc (
-    inout osc_io,
-    input osc_rst,
-    input osc_halt,
-    output reg [31:0] osc_counter
+    input  ref_clk,
+    inout  osc_io,
+    input  osc_rst,
+    input  osc_halt,
+    input  osc_latch_req,
+    output reg osc_latch_ack,
+    output reg [31:0] osc_counter_latch
 );
-    wire io_lb;
+    (* clkbuf_inhibit *)
+    wire osc_lb;
 
     CC_IOBUF #(
-        .DRIVE("12"),         // "3", "6", "9" or "12" mA
+        .DRIVE("12"),        // "3", "6", "9" or "12" mA
         .SLEW("SLOW"),       // "SLOW" or "FAST"
         .PULLUP(0),          // 0: disable, 1: enable
         .PULLDOWN(0),        // 0: disable, 1: enable
@@ -20,22 +24,57 @@ module osc (
         .FF_IBF(1'b0),       // 0: disable, 1: enable
         .FF_OBF(1'b0)        // 0: disable, 1: enable
     ) iobuf_inst (
-        .A(~io_lb),
-        .T(osc_halt), // 0: output, 1: input
-        .Y(io_lb),
+        .A(~osc_lb),
+        .T(osc_halt | osc_rst), // 0: output, 1: input
+        .Y(osc_lb),
         .IO(osc_io)
     );
 
-    initial osc_counter = 0;
+    reg [31:0] osc_counter = 0;
+    initial osc_counter_latch = 0;
 
     // ringosc counter
-    always @(posedge io_lb or posedge osc_rst)
+    always @(posedge osc_lb or posedge osc_rst)
     begin
         if (osc_rst == 1) begin
             osc_counter <= 0;
         end
-        else begin
+        else if (!osc_latch_req) begin
             osc_counter <= osc_counter + 1'b1;
         end
     end
+
+    // Synchronize latch_req to osc_lb domain
+    reg latch_req_sync1 = 0, latch_req_sync2 = 0;
+    always @(posedge osc_lb or posedge osc_rst) begin
+        if (osc_rst == 1) begin
+            latch_req_sync1 <= 0;
+            latch_req_sync2 <= 0;
+        end else begin
+            latch_req_sync1 <= osc_latch_req;
+            latch_req_sync2 <= latch_req_sync1;
+        end
+    end
+
+    // Latch osc_counter when osc_latch_req is detected
+    always @(posedge osc_lb or posedge osc_rst) begin
+        if (osc_rst == 1) begin
+            osc_counter_latch <= 0;
+        end else if (latch_req_sync2) begin
+            osc_counter_latch <= osc_counter;
+        end
+    end
+
+    // Synchronize latch_ack back to ref_clk domain
+    reg latch_ack_sync1 = 0;
+    always @(posedge ref_clk or posedge osc_rst) begin
+        if (osc_rst) begin
+            latch_ack_sync1 <= 0;
+            osc_latch_ack <= 0;
+        end else begin
+            latch_ack_sync1 <= latch_req_sync2;
+            osc_latch_ack <= latch_ack_sync1;
+        end
+    end
+
 endmodule
--- rtl/top.v
+++ rtl/top.v
@@ -21,48 +21,86 @@ module top #(
 )(
     input wire  ref_clk,
     inout wire  osc_io_2v5,
-    inout wire  osc_io_3v3,
+    inout wire  osc_io_3v6,
     output wire uart_tx,
     output wire uart_tx_done_n,
     output wire uart_tx_busy_n,
     output wire osc_halt
 );
+    // reset
+    reg [5:0] rst_cnt = 0;
+    wire rstn = &rst_cnt;
+    wire rst = !rstn;
+
+    always @(posedge ref_clk) begin
+        rst_cnt <= rst_cnt + !rstn;
+    end
 
     wire [31:0] osc_counter_2v5;
-    wire [31:0] osc_counter_3v3;
+    wire [31:0] osc_counter_3v6;
+
+    wire osc_latch_ack_2v5, osc_latch_ack_3v6;
+    wire [31:0] osc_counter_latch_2v5, osc_counter_latch_3v6;
 
     osc osc_inst0 (
+        .ref_clk(ref_clk),
         .osc_io(osc_io_2v5),
         .osc_rst(osc_rst),
         .osc_halt(osc_halt),
-        .osc_counter(osc_counter_2v5)
+        .osc_latch_req(latch_req),
+        .osc_latch_ack(osc_latch_ack_2v5),
+        .osc_counter_latch(osc_counter_latch_2v5)
     );
 
     osc osc_inst1 (
-        .osc_io(osc_io_3v3),
+        .ref_clk(ref_clk),
+        .osc_io(osc_io_3v6),
         .osc_rst(osc_rst),
         .osc_halt(osc_halt),
-        .osc_counter(osc_counter_3v3)
+        .osc_latch_req(latch_req),
+        .osc_latch_ack(osc_latch_ack_3v6),
+        .osc_counter_latch(osc_counter_latch_3v6)
     );
 
     reg [31:0] ref_counter = 0;
+    reg latch_req = 0;
+    reg done = 0;
     reg [2*32-1:0] txd;
 
-    // 1 sec sample
-    always @(posedge ref_clk)
+    // generate sample latch request
+    always @(posedge ref_clk or posedge rst)
     begin
-        ref_counter <= ref_counter + 1'b1;
-        if (ref_counter == REF_CLK) begin
+        if (rst == 1'b1) begin
             ref_counter <= 0;
-            txd <= {osc_counter_3v3, osc_counter_2v5};
+            latch_req <= 0;
+            done <= 0;
+            txd <= 0;
+        end
+        else begin
+            if (!done) begin
+                if (ref_counter < REF_CLK) begin
+                    ref_counter <= ref_counter + 1'b1;
+                end
+                else begin
+                    done <= 1;
+                    latch_req <= 1'b1;
+                end
+            end
+            // Transfer latched value
+            if ((osc_latch_ack_2v5 & osc_latch_ack_3v6) || (done & osc_halt)) begin
+                latch_req <= 1'b0;
+                done <= 0;
+                ref_counter <= 0;
+                txd <= {osc_counter_latch_3v6, osc_counter_latch_2v5};
+            end
         end
     end
 
     (* clkbuf_inhibit *)
-    wire stp_clk = (ref_counter == 0);
+    wire stp_clk = (ref_counter == REF_CLK);
     reg [$clog2(STP_SMPL)-1:0] stp_counter = 0;
 
-    // stop counter: each 20 samples
+    // stop counter: halt oscillator after `STP_SMPL` samples
     always @(posedge stp_clk)
     begin
         stp_counter <= stp_counter + 1'b1;
@@ -88,7 +126,7 @@ module top #(
         .STOP(1)
     ) tx_inst (
         .clk_i(ref_clk),
-        .rst_i(1'b0),
+        .rst_i(rst),
         .tx_start_i(ref_counter == 0),
         .tx_data_i(txd),
         .tx_done_o(uart_tx_done),
