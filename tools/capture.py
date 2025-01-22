21d8efc50c53512daa329b522e3d2c797206dc6e
Patrick Urban <patrick.urban@web.de>
2025-01-22 11:41:47 +0100

    tools/capture.py: change from 3v3 to 3v6

--- tools/capture.py
+++ tools/capture.py
@@ -54,17 +54,17 @@ def main():
 
     try:
         while running:
-            value3v3, value2v5 = read_uart_data(ser)
-            if value3v3 is not None and value2v5 is not None:
+            value3v6, value2v5 = read_uart_data(ser)
+            if value3v6 is not None and value2v5 is not None:
                 elapsed_time = time.time() - start_time
                 current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
                 # Write timestamp and value to file
-                file.write(f"{current_time}; {value3v3}; {value2v5}\n")
+                file.write(f"{current_time}; {value3v6}; {value2v5}\n")
 
                 # Update data for plotting
                 x_data.append(elapsed_time)
-                y1_data.append(value3v3 / 1e6)
+                y1_data.append(value3v6 / 1e6)
                 y2_data.append(value2v5 / 1e6)
                 if len(x_data) > max_points:
                     x_data.pop(0)
@@ -73,11 +73,11 @@ def main():
 
                 # Update plot
                 ax.clear()
-                ax.plot(x_data, y1_data, marker='', linestyle='-', color='b', label='3V3 Osc. (MHz)')
+                ax.plot(x_data, y1_data, marker='', linestyle='-', color='b', label='3V6 Osc. (MHz)')
                 ax.plot(x_data, y2_data, marker='', linestyle='-', color='r', label='2V5 Osc. (MHz)')
                 ax.set_title("Real-time UART Data")
                 ax.set_xlabel("Time (s)")
-                ax.set_ylabel("Value (MHz)")
+                ax.set_ylabel("Osc. Value (MHz)")
                 ax.legend()
                 ax.grid(True, linestyle=':')
                 plt.pause(0.01)  # Small delay to update the plot
