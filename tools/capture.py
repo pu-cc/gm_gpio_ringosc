2638f81ef85a66d78fd3637db6349fcd4e2aa73e
Patrick Urban <patrick.urban@web.de>
2025-02-21 01:00:58 +0100

    Test log file for 2025-02-21

--- tools/capture.py
+++ tools/capture.py
@@ -1,9 +1,11 @@
+import os
 import serial
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 import struct
 import time
+import subprocess
 from datetime import datetime
 
 # Use the 'tkagg' backend for non-interactive plotting
@@ -12,6 +14,15 @@ matplotlib.use('tkagg')
 # UART configuration
 PORT = '/dev/ttyUSB1'
 BAUDRATE = 115200
+
+GITHUB_USER  = os.getenv("GITHUB_USERNAME")
+GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
+
+if not GITHUB_USER or not GITHUB_TOKEN:
+    print("Error: GitHub credentials not found. Set GITHUB_USERNAME and GITHUB_TOKEN as environment variables.")
+    exit(1)
+
+GIT_REPO_URL = f"https://{os.getenv('GITHUB_USERNAME')}:{GITHUB_TOKEN}@github.com/pu-cc/gm_gpio_ringosc.git"
 
 def read_uart_data(serial_port):
     """Reads 8 bytes from the UART and returns a 32-bit integer."""
@@ -31,6 +42,25 @@ def compensate_temperature(raw_temp24, dig_T1=0x6D42, dig_T2=0x6877, dig_T3=0xFC
 
     return temperature
 
+def get_log_file():
+    today = datetime.now().strftime("%Y-%m-%d")
+    return f"db/log_{today}.csv"
+
+def push_to_github(file_path):
+    try:
+        subprocess.run(["git", "add", file_path], check=True)
+
+        today = datetime.datetime.now().strftime("%Y-%m-%d")
+        commit_message = f"Log file for {today}"
+        subprocess.run(["git", "commit", "-m", commit_message], check=True)
+
+        subprocess.run(["git", "remote", "set-url", "origin", GIT_REPO_URL], check=True)
+        subprocess.run(["git", "push", "origin", "main"], check=True)
+
+    except subprocess.CalledProcessError as e:
+        print(f"Failed to push to github: {e}")
+        exit(1)
+
 def main():
     # Open the UART connection
     try:
@@ -39,15 +69,6 @@ def main():
         print(f"Connected to {PORT} at {BAUDRATE} baud.")
     except serial.SerialException as e:
         print(f"Failed to connect: {e}")
-        return
-
-    # Open a file for writing
-    file_name = f"db/uart_data{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.txt"
-    try:
-        file = open(file_name, "w", buffering=1)
-        print(f"Writing data to {file_name}.")
-    except IOError as e:
-        print(f"Failed to open file: {e}")
         return
 
     # Setup for real-time plotting
@@ -80,47 +101,63 @@ def main():
 
     fig.canvas.mpl_connect('close_event', on_close)
 
+    last_log_file = ''
+
     try:
         while running:
-            value3v6, value2v5, raw_temp24 = read_uart_data(ser)
-            if value3v6 is not None and value2v5 is not None and raw_temp24 is not None:
-                ctemp = compensate_temperature(raw_temp24) / 100.0
+            # Open a file for writing
+            log_file = get_log_file()
+            log_exists = os.path.exists(log_file)
 
-                elapsed_time = time.time() - start_time
-                current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            if not log_exists:
+                if last_log_file != '':
+                    push_to_github(last_log_file)
+                else:
+                    last_log_file = log_file
 
-                # Write timestamp and value to file
-                file.write(f"{current_time}; {value3v6}; {value2v5}; {ctemp}\n")
+            with open(log_file, mode='a', newline='', buffering=1) as file:
+                value3v6, value2v5, raw_temp24 = read_uart_data(ser)
+                if value3v6 is not None and value2v5 is not None and raw_temp24 is not None:
+                    ctemp = compensate_temperature(raw_temp24) / 100.0
 
-                # Update data for plotting
-                x_data.append(elapsed_time)
-                y1_data.append(value3v6 / 1e6)
-                y2_data.append(value2v5 / 1e6)
-                y3_data.append(ctemp)
-                if len(x_data) > max_points:
-                    x_data.pop(0)
-                    y1_data.pop(0)
-                    y2_data.pop(0)
-                    y3_data.pop(0)
+                    elapsed_time = time.time() - start_time
+                    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-                # Update plot lines
-                line1.set_xdata(x_data)
-                line1.set_ydata(y1_data)
-                line2.set_xdata(x_data)
-                line2.set_ydata(y2_data)
-                line3.set_xdata(x_data)
-                line3.set_ydata(y3_data)
+                    # Write timestamp and value to file
+                    file.write(f"{current_time}; {value3v6}; {value2v5}; {ctemp}\n")
 
-                # Rescale axes to fit the data
-                ax1.relim()
-                ax1.autoscale_view()
-                ax2.relim()
-                ax2.autoscale_view()
+                    # Update data for plotting
+                    x_data.append(elapsed_time)
+                    y1_data.append(value3v6 / 1e6)
+                    y2_data.append(value2v5 / 1e6)
+                    y3_data.append(ctemp)
+                    if len(x_data) > max_points:
+                        x_data.pop(0)
+                        y1_data.pop(0)
+                        y2_data.pop(0)
+                        y3_data.pop(0)
 
-                plt.pause(0.01)
+                    # Update plot lines
+                    line1.set_xdata(x_data)
+                    line1.set_ydata(y1_data)
+                    line2.set_xdata(x_data)
+                    line2.set_ydata(y2_data)
+                    line3.set_xdata(x_data)
+                    line3.set_ydata(y3_data)
+
+                    # Rescale axes to fit the data
+                    ax1.relim()
+                    ax1.autoscale_view()
+                    ax2.relim()
+                    ax2.autoscale_view()
+
+                    plt.pause(0.01)
 
     except KeyboardInterrupt:
         print("Exiting...")
+
+    except IOError as e:
+        print(f"Failed to open file: {e}")
 
     finally:
         ser.close()
